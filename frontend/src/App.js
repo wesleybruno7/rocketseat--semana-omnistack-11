5c41b3860451f419992d933b89a53b5b1a95aeec
wesleybruno7 <wesleybruno7@terra.com.br>
2020-03-25 11:53:20 -0300

    Concluido 3a video aula
    - concluido a aplicação web (front-end)
    - integrado com a api (back-end)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from './Header'
+import './global.css'
+
+import Routes from './routes'
 
 function App() {
-  // Array [valor, funcaoDeAtaulizacao]
-  const [counter, setCounter] = useState(0)
-
-  function increment() {
-    setCounter(counter + 1)
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Routes />
   )
 }
 
